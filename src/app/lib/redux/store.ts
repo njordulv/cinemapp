d7ea81444d30a434e0465d270e821a760b5733ac
njordulv <njordulv@gmail.com>
2024-03-14 11:42:57 +0200

    ADD: redux persist local storage provider

--- src/app/lib/redux/store.ts
+++ src/app/lib/redux/store.ts
@@ -1,17 +1,67 @@
-import { configureStore } from '@reduxjs/toolkit'
-import movieReducer from '@/redux/slices/movieSlice'
-import tvReducer from '@/redux/slices/tvSlice'
+import { configureStore, combineReducers } from '@reduxjs/toolkit'
+import {
+  persistStore,
+  persistReducer,
+  FLUSH,
+  REHYDRATE,
+  PAUSE,
+  PERSIST,
+  PURGE,
+  REGISTER,
+} from 'redux-persist'
+import createWebStorage from 'redux-persist/lib/storage/createWebStorage'
 import {
   useSelector as useReduxSelector,
   useDispatch as useReduxDispatch,
 } from 'react-redux'
+import movieReducer from '@/redux/slices/movieSlice'
+import tvReducer from '@/redux/slices/tvSlice'
 
-export const reduxStore = configureStore({
-  reducer: {
-    movie: movieReducer,
-    tv: tvReducer,
-  },
+const rootReducer = combineReducers({
+  movie: movieReducer,
+  tv: tvReducer,
 })
 
+const createNoopStorage = () => {
+  return {
+    getItem(_key: string) {
+      return Promise.resolve(null)
+    },
+    setItem(_key: string, value: any) {
+      return Promise.resolve(value)
+    },
+    removeItem(_key: string) {
+      return Promise.resolve()
+    },
+  }
+}
+
+const storage =
+  typeof window !== 'undefined'
+    ? createWebStorage('local')
+    : createNoopStorage()
+
+export default storage
+
+const persistConfig = {
+  key: 'root',
+  storage,
+}
+
+const persistedReducer = persistReducer(persistConfig, rootReducer)
+
+export const reduxStore = configureStore({
+  reducer: persistedReducer,
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+      },
+    }),
+})
+
+export const persistor = persistStore(reduxStore)
+
+export type AppDispatch = typeof reduxStore.dispatch
 export const useDispatch = () => useReduxDispatch()
 export const useSelector = useReduxSelector
