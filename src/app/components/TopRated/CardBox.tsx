4e65322996c8151ff56c0176f3817b37aac0a682
njordulv <njordulv@gmail.com>
2024-03-24 20:22:41 +0200

    UPD: top rated fetch api & cardbox tpl

--- src/app/components/TopRated/CardBox.tsx
+++ src/app/components/TopRated/CardBox.tsx
@@ -1,10 +1,5 @@
-import { useState, useEffect } from 'react'
-import { Card, CardFooter, Image } from '@nextui-org/react'
-import { useRouter } from 'next/navigation'
 import { TopRated } from '@/types/data'
-import { formatReleaseDate } from '@/utils/formatDate'
-import VoteAverage from '@/components/UI/VoteAverage'
-import VoteDisabled from '@/components/UI/VoteDisabled'
+import MainCard from '@/components/UI/MainCard'
 
 export default function CardBox({
   id,
@@ -13,78 +8,16 @@ export default function CardBox({
   release_date,
   vote_average,
 }: TopRated) {
-  const router = useRouter()
-  const formattedReleaseDate = formatReleaseDate(release_date)
-  const [error, setError] = useState<string | null>(null)
-  const [topRatedDetails, setTopRatedDetails] = useState(null)
-  const NO_IMAGE = '/no-image.svg'
-
-  useEffect(() => {
-    fetch(
-      `api/movies?endpoint=movie/top_rated/${id}&combinedEndpoints=top_rated/${id}/credits`
-    )
-      .then((response) => {
-        if (!response.ok) {
-          throw new Error('Failed to fetch data')
-        }
-        return response.json()
-      })
-      .then((data) => {
-        setTopRatedDetails(data)
-      })
-      .catch((error) => setError(error.message))
-  }, [id])
-
-  const pageHandler = () => {
-    router.push(`/movie/${id}`)
-  }
-
-  if (error) {
-    return <div>There is an error - {error}</div>
-  }
-
   return (
-    <Card
-      isFooterBlurred
-      isPressable
-      radius="lg"
-      className="border-none bg-content-none bg-blueDark"
-      onPress={pageHandler}
-    >
-      <Image
-        className="object-cover"
-        src={image}
-        width={220}
-        height={330}
-        fallbackSrc={NO_IMAGE}
-        alt={title}
+    <>
+      <MainCard
+        type={'movie'}
+        id={id}
+        image={image}
+        name={title}
+        date={release_date}
+        vote_average={vote_average}
       />
-      <CardFooter className="p-3 py-1 h-auto flex flex-col items-start text-left color-inherit subpixel-antialiased bg-background/10 backdrop-blur-[2px] backdrop-saturate-100 backdrop-contrast-125 before:bg-white/10 border-white/20 border-1 overflow-hidden absolute before:rounded-xl rounded-large bottom-1 w-[calc(100%_-_8px)] shadow-small ml-1 z-10">
-        <div className="text-white text-[14px] leading-[18px] text-shadow-sm pr-8">
-          {title}
-        </div>
-        <div className="text-tiny text-white/80 text-shadow-sm">
-          {formattedReleaseDate}
-        </div>
-        <div className="absolute top-[50%] right-[3px] mt-[-20px]">
-          {vote_average ? (
-            <VoteAverage
-              vote={vote_average}
-              card="w-[40px] h-[40px]"
-              size="w-8 h-8 drop-shadow-md"
-              strokeWidth={2}
-              text="text-[12px]"
-            />
-          ) : (
-            <VoteDisabled
-              card="w-[40px] h-[40px]"
-              size="w-8 h-8 drop-shadow-md"
-              strokeWidth={2}
-              text="text-[12px] top-[-3px] relative"
-            />
-          )}
-        </div>
-      </CardFooter>
-    </Card>
+    </>
   )
 }
--- src/app/components/TopRated/Layout.tsx
+++ src/app/components/TopRated/Layout.tsx
@@ -1,51 +1,37 @@
 'use client'
 
-import { useState, useEffect } from 'react'
+import { useState } from 'react'
 import { TopRated } from '@/types/data'
+import useFetcher from '@/hooks/useFetcher'
 import SkeletonBox from '@/components/UI/SkeletonBox'
 import CardBox from '@/components/TopRated/CardBox'
 import Paginate from '@/components/UI/Paginate'
-
-const BASE_IMAGE_URL = 'https://image.tmdb.org/t/p/w500'
-const NO_IMAGE = '/no-image.svg'
+import Error from '@/components/UI/Error'
 
 export default function Layout() {
-  const [topRated, setTopRated] = useState<TopRated[]>([])
-  const [loading, setLoading] = useState(true)
-  const [error, setError] = useState('')
   const [currentPage, setCurrentPage] = useState(1)
+  const { data, isError, isLoading } = useFetcher({
+    endpoint: `/api/movies?endpoint=movie/top_rated&page=${currentPage}`,
+  })
+  const NO_IMAGE = '/no-image.svg'
 
-  useEffect(() => {
-    fetch(`/api/movies?endpoint=movie/top_rated&page=${currentPage}`)
-      .then((response) => {
-        if (!response.ok) {
-          throw new Error('Failed to fetch data')
-        }
-        return response.json()
-      })
-      .then((data) => setTopRated(data.results))
-      .catch((error) => setError(error.message))
-      .finally(() => setLoading(false))
-  }, [currentPage])
+  if (isError) return <Error errorText={isError.message} />
 
   return (
     <>
-      {error && <h1 className="heading">This is an error - {error}</h1>}
       <div className="mb-32 grid text-center lg:max-w-[1170px] lg:w-full lg:mb-0 lg:grid-cols-5 lg:text-left gap-4">
-        {loading ? (
+        {isLoading ? (
           <>
             {[...Array(20)].map((_, index) => (
               <SkeletonBox key={index} />
             ))}
           </>
         ) : (
-          topRated.map((topRated) => (
+          data.results.map((topRated: TopRated) => (
             <CardBox
               {...topRated}
               image={
-                topRated.poster_path
-                  ? `${BASE_IMAGE_URL}${topRated.poster_path}`
-                  : NO_IMAGE
+                topRated.poster_path ? `${topRated.poster_path}` : NO_IMAGE
               }
               key={topRated.id}
             />
