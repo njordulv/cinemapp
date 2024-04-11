478626b2b07503e2a416f4973de659819821d01f
njordulv <njordulv@gmail.com>
2024-04-12 01:21:14 +0300

    ADD: firebase env

--- config/firebase.ts
+++ config/firebase.ts
@@ -2,12 +2,12 @@ import { initializeApp } from 'firebase/app'
 import { getAuth } from 'firebase/auth'
 
 const firebaseConfig = {
-  apiKey: 'AIzaSyA0urvD4B_pIbx984vTwGpke89cjMVl6rw',
-  authDomain: 'cinemapp-auth.firebaseapp.com',
-  projectId: 'cinemapp-auth',
-  storageBucket: 'cinemapp-auth.appspot.com',
-  messagingSenderId: '152185057179',
-  appId: '1:152185057179:web:a1d2a303d9d241a6acfb3e',
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
 }
 
 const app = initializeApp(firebaseConfig)
