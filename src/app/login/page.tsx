9ce529b705a2bc0750d9fcc7a937d32ed24c8f9e
njordulv <njordulv@gmail.com>
2024-04-24 19:56:51 +0300

    FIX: height for login & register

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -3,7 +3,7 @@ import { Link } from '@nextui-org/react'
 
 const LoginPage = () => {
   return (
-    <main className="flex flex-col items-center place-content-center min-h-96 h-[100vh] w-full max-w-[1170px] m-auto px-4 py-10 gap-5">
+    <main className="flex flex-col items-center place-content-center min-h-96 h-[calc(100vh_-_65px)] w-full max-w-[1170px] m-auto px-4 py-10 gap-5">
       <Login />
       <div className="text-medium">
         Or <Link href="/register">Register</Link>
--- src/app/register/page.tsx
+++ src/app/register/page.tsx
@@ -3,7 +3,7 @@ import { Link } from '@nextui-org/react'
 
 const RegisterPage = () => {
   return (
-    <main className="flex flex-col items-center place-content-center min-h-96 h-[100vh] w-full max-w-[1170px] m-auto px-4 py-10 gap-5">
+    <main className="flex flex-col items-center place-content-center min-h-96 h-[calc(100vh_-_65px)] w-full max-w-[1170px] m-auto px-4 py-10 gap-5">
       <Register />
       <div className="text-medium">
         Already have an account? <Link href="/login">Sign in</Link>
