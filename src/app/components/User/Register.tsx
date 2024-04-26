69c0ce3a0a73517a85c4ec3ee07427b302f8c4a0
njordulv <njordulv@gmail.com>
2024-04-26 17:41:02 +0300

    UPD: register page with firebase

--- src/app/components/User/Register.tsx
+++ src/app/components/User/Register.tsx
@@ -2,10 +2,11 @@
 
 import { useRouter } from 'next/navigation'
 import { createUserWithEmailAndPassword } from 'firebase/auth'
+import { getFirestore, doc, setDoc } from 'firebase/firestore'
 import { useAppDispatch, useAppSelector } from '@/hooks/reduxHooks'
+import { auth } from '@/config/firebase'
 import { setUser } from '@/redux/slices/userSlice'
 import { selectErrorMessage, setError } from '@/redux/slices/errorSlice'
-import { auth } from '@/config/firebase'
 import Form from '@/components/User/Form'
 
 const Register = () => {
@@ -13,23 +14,37 @@ const Register = () => {
   const router = useRouter()
   const error = useAppSelector(selectErrorMessage)
 
-  const handleRegister = (email: string, password: string) => {
-    createUserWithEmailAndPassword(auth, email, password)
-      .then(({ user }) => {
-        dispatch(
-          setUser({
-            email: user.email,
-            id: user.uid,
-            token: user.refreshToken,
-          })
-        )
-        router.push('/dashboard')
+  const handleRegister = async (email: string, password: string) => {
+    try {
+      const userCredential = await createUserWithEmailAndPassword(
+        auth,
+        email,
+        password
+      )
+      const user = userCredential.user
+      const firestore = getFirestore()
+      const userDocRef = doc(firestore, 'users', user.uid)
+
+      await setDoc(userDocRef, {
+        email: user.email,
+        createdAt: user.metadata.creationTime,
       })
-      .catch((error) => {
-        const errorMessage =
-          error.message || 'An error occurred during register.'
-        dispatch(setError(errorMessage))
-      })
+
+      dispatch(
+        setUser({
+          email: user.email,
+          id: user.uid,
+          token: user.refreshToken,
+          createdAt: user.metadata.creationTime,
+        })
+      )
+
+      router.push('/dashboard')
+    } catch (error: any) {
+      const errorMessage =
+        error.message || 'An error occurred during registration.'
+      dispatch(setError(errorMessage))
+    }
   }
 
   return <Form title="Register" error={error} handleClick={handleRegister} />
